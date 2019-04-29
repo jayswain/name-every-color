0f8945ea2054412acdb65a6c87d2aee25920bbc4
Jay Swain <github@jayswain.com>
2019-04-29 00:23:16 -0700

    Status class
    IT WORKS! WOOHOO!
    
    TODO: swap out https for request lib

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Bearer = require('./twitter/bearer');
 const Timeline = require('./twitter/timeline');
+const Status = require('./twitter/status');
 const ntc = require('./vendor/ntc/ntc');
 
 const screenName = 'everycolorbot';
@@ -11,10 +12,21 @@ Bearer.requestToken(process.env.TWITTER_CONSUMER_API_KEY, process.env.TWITTER_CO
 
   Timeline.request(bearerToken, screenName).then(response => {
     response.forEach((tweet) => {
+      const id = tweet.id_str;
       const text = tweet.text.split(' ')[0];
       const hex = `#${ text.split(" ")[0].slice(2, 8) }`;
-      const colorName = ntc.name(hex);
-      console.log(colorName);
+      const colorName = ntc.name(hex)[1];
+      const status = `@${ screenName } ${ colorName }`;
+      Status.update(
+        process.env.TWITTER_CONSUMER_API_KEY,
+        process.env.TWITTER_CONSUMER_API_SECRET,
+        process.env.TWITTER_ACCOUNT_ACCESS_TOKEN,
+        process.env.TWITTER_ACCOUNT_ACCESS_TOKEN_SECRET,
+        status,
+        {
+          in_reply_to_status_id: id
+        }
+      );
     });
   }).catch(error => {
     console.log('error', error);
---
+++ twitter/status.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+class Status {
+  static update(consumerAPIKey, consumerAPISecret, clientAccessToken, clientAccessSecret, message, qsParams) {
+    return new Promise((resolve, reject) => {
+      request.post({
+        url: `https://api.twitter.com/1.1/statuses/update.json`,
+        oauth: {
+          consumer_key: consumerAPIKey,
+          consumer_secret: consumerAPISecret,
+          token: clientAccessToken,
+          token_secret: clientAccessSecret
+        },
+        qs: {
+          ...qsParams,
+          status: message
+        }
+      }, (err, response, body) => {
+        console.log('err', err)
+        console.log('resp', response)
+        console.log('body', body);
+      });
+    });
+  };
+}
+
+module.exports = Status;
