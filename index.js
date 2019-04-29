7cae6a95be4f41ba77208c22c4631eb1efd70f73
Jay Swain <github@jayswain.com>
2019-04-28 21:06:19 -0700

    name color from tweet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Bearer = require('./twitter/bearer');
 const Timeline = require('./twitter/timeline');
+const ntc = require('./vendor/ntc/ntc');
 
 const screenName = 'everycolorbot';
 
@@ -9,7 +10,12 @@ Bearer.requestToken(process.env.TWITTER_CONSUMER_API_KEY, process.env.TWITTER_CO
   const bearerToken = response;
 
   Timeline.request(bearerToken, screenName).then(response => {
-    console.log(response);
+    response.forEach((tweet) => {
+      const text = tweet.text.split(' ')[0];
+      const hex = `#${ text.split(" ")[0].slice(2, 8) }`;
+      const colorName = ntc.name(hex);
+      console.log(colorName);
+    });
   }).catch(error => {
     console.log('error', error);
   });
