8107204c825a8242ec1d885188d07a59bd3b035e
Jay Swain <github@jayswain.com>
2019-05-05 14:58:09 -0700

    move logic to app and run method

---
+++ app.js
@@ -0,0 +1,57 @@
+const ntc = require('./vendor/ntc');
+const Twitter = require('./twitter');
+
+const TARGET_SCREEN_NAME = 'everycolorbot';
+const BOT_SCREEN_NAME = 'colornamebot';
+
+class App {
+  static run() {
+    console.log('running');
+    let bearerToken;
+
+    Twitter.Bearer.requestToken(process.env.TWITTER_CONSUMER_API_KEY, process.env.TWITTER_CONSUMER_API_SECRET).then(response => {
+      bearerToken = response;
+      return response;
+    }).then(response => {
+      //find the id of the last tweet our bot responded to
+      return Twitter.Timeline.request(bearerToken, BOT_SCREEN_NAME, { count: 1 }).then(response => {
+        return response[0]["in_reply_to_status_id"];
+      })
+    }).then(sinceId => {
+      //find the tweets of the target bot since we last responded
+      return Twitter.Timeline.request(bearerToken, TARGET_SCREEN_NAME, { since_id: sinceId, count: 200 });
+    }).then(tweets => {
+      //Using a reverse loop here so our sinceId lines up for the *next* run.
+      //
+      //since_id param *includes* that id as well, so lets skip that one.
+      //
+      for(let i = tweets.length - 1; i--;) {
+        const tweet = tweets[i];
+        const id = tweet.id_str;
+        const text = tweet.text.split(' ')[0];
+        const hex = `#${ text.split(" ")[0].slice(2, 8) }`;
+        const colorName = ntc.name(hex)[1];
+        const status = `@${ TARGET_SCREEN_NAME } ${ colorName }`;
+
+        console.log('.');
+
+        //Twitter.Status.update(
+          //process.env.TWITTER_CONSUMER_API_KEY,
+          //process.env.TWITTER_CONSUMER_API_SECRET,
+          //process.env.TWITTER_ACCOUNT_ACCESS_TOKEN,
+          //process.env.TWITTER_ACCOUNT_ACCESS_TOKEN_SECRET,
+          //status,
+          //{
+            //in_reply_to_status_id: id
+          //}
+        //);
+      }
+    }).then(() => {
+      console.log('finished');
+    }).catch(error => {
+      console.log('error', error);
+    });
+  }
+}
+
+module.exports = App;
--- index.js
+++ index.js
@@ -1,49 +1,5 @@
 require('dotenv').config();
 
-const Twitter = require('./twitter');
-const ntc = require('./vendor/ntc');
+const App = require('./app');
 
-const targetScreenName = 'everycolorbot';
-const botScreenName = 'colornamebot';
-let bearerToken;
-
-Twitter.Bearer.requestToken(process.env.TWITTER_CONSUMER_API_KEY, process.env.TWITTER_CONSUMER_API_SECRET).then(response => {
-  bearerToken = response;
-  return response;
-}).then(response => {
-  //find the id of the last tweet our bot responded to
-  return Twitter.Timeline.request(bearerToken, botScreenName, { count: 1 }).then(response => {
-    return response[0]["in_reply_to_status_id"];
-  })
-}).then(sinceId => {
-  //find the tweets of the target bot since we last responded
-  return Twitter.Timeline.request(bearerToken, targetScreenName, { since_id: sinceId, count: 200 });
-}).then(tweets => {
-  //Using a reverse loop here so our sinceId lines up for the *next* run.
-  //
-  //since_id param *includes* that id as well, so lets skip that one.
-  //
-  for(let i = tweets.length - 1; i--;) {
-    const tweet = tweets[i];
-    const id = tweet.id_str;
-    const text = tweet.text.split(' ')[0];
-    const hex = `#${ text.split(" ")[0].slice(2, 8) }`;
-    const colorName = ntc.name(hex)[1];
-    const status = `@${ targetScreenName } ${ colorName }`;
-
-    console.log(status);
-
-    //Twitter.Status.update(
-      //process.env.TWITTER_CONSUMER_API_KEY,
-      //process.env.TWITTER_CONSUMER_API_SECRET,
-      //process.env.TWITTER_ACCOUNT_ACCESS_TOKEN,
-      //process.env.TWITTER_ACCOUNT_ACCESS_TOKEN_SECRET,
-      //status,
-      //{
-        //in_reply_to_status_id: id
-      //}
-    //);
-  }
-}).catch(error => {
-  console.log('error', error);
-});
+App.run();
